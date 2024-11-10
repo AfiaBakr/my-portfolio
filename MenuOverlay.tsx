62bf387760eb0fa40ad48d818d7eb4ab291ad318
AfiaBakr <159829947+AfiaBakr@users.noreply.github.com>
2024-11-10 23:11:11 +0500

    commit

--- MenuOverlay.tsx
+++ MenuOverlay.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavLink from "./NavLink";
 
-export default function MenuOverlay ({links:any}){
+const MenuOverlay = ({links}:any)=>{
     return(
         <ul className="flex flex-col py-4 items-center">
-            <li key={index}>
-            {links.map((link, index)=>(<NavLink href={link.path} title={link.title}/>))}
+            <li key={"index"}>
+            {links.map((link: { path: any; title: any; }, index: any)=>(<NavLink href={link.path} title={link.title}/>))}
             </li>
         </ul>
     )
 }
+
+export default MenuOverlay;
